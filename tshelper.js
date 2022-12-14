0bd101659b413ea3906c5ba3a2ca19ce8d44ba1a
ThinhHuynh <minhthinhlss@gmail.com>
2022-12-14 21:03:04 +0700

    > Upgrade `tsconfig` and `tshelper` to resovle module `@types`.

--- tshelper.js
+++ tshelper.js
@@ -4,60 +4,73 @@
 
 "use strict";
 
-const __main__ = () => {
-    const ignores = ['model', 'mongoose'];
-
-    const exports = (ignores = ignores) => {
-        const config = {
-            watchDirs: {
-                model: {
-                    directory: 'app/model', // files directory.
-                    pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
-                    ignore: '', // ignore glob pattern, default to empty.
-                    generator: 'function', // generator name, eg: class、auto、function、object
-                    interface: 'IModel', // interface name
-                    declareTo: 'Context.model', // declare to this interface
-                    watch: true, // whether need to watch files
-                    caseStyle: 'upper', // caseStyle for loader
-                    interfaceHandle: Interface => `ReturnType<typeof ${Interface}>`, // interfaceHandle
-                    trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
-                },
-                mongoose: {
-                    directory: 'app/mongoose', // files directory.
-                    pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
-                    ignore: '', // ignore glob pattern, default to empty.
-                    generator: 'function', // generator name, eg: class、auto、function、object
-                    interface: 'IMongoose', // interface name
-                    declareTo: 'Context.mongoose', // declare to this interface
-                    watch: true, // whether need to watch files
-                    caseStyle: 'upper', // caseStyle for loader
-                    interfaceHandle: Interface => `ReturnType<typeof ${Interface}>`, // interfaceHandle
-                    trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
-                },
-                service: {
-                    directory: 'app/service', // files directory.
-                    pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
-                    ignore: '', // ignore glob pattern, default to empty.
-                    generator: 'class', // generator name, eg: class、auto、function、object
-                    interface: 'IService', // interface name
-                    declareTo: 'Context.service', // declare to this interface
-                    watch: true, // whether need to watch files
-                    caseStyle: 'lower', // caseStyle for loader
-                    interfaceHandle: Interface => `${Interface}`, // Interface Handler
-                    trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
-                }
-            },
-            autoRemoveJs: true,
-        };
-
-        for (const ignore of ignores) {
-            delete config.watchDirs[ignore];
+/**
+ ** @description - Remove some specified properties from source object.
+ ** @template T
+ ** @param {Array<string>} keys
+ ** @param {T} object
+ ** @returns {Partial<T>}
+ **/
+const omit = (keys, object) => {
+    if (!keys) return object;
+    const numKeys = keys.length;
+    const destObj = Object.assign({}, object);
+    for (let i = 0; i < numKeys; i++) {
+        let curr = destObj;
+        const subKeys = keys[i].split('.'); // Ex: [1,2,3]
+        const lastKey = subKeys.splice(-1); // Ex: ${lastKey}==3; ${subKeys}==[1,2]
+        for (const subKey of subKeys) {
+            curr = curr[subKey];
         }
+        delete curr[lastKey];
+    }
+    return destObj;
+};
 
-        return config;
-    };
+const __main__ = () => {
+    const exports = (removeKeys) => omit(removeKeys, {
+        generatorConfig: {
+            model: {
+                directory: 'app/model', // files directory.
+                pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
+                ignore: '', // ignore glob pattern, default to empty.
+                generator: 'function', // generator name, eg: class、auto、function、object
+                interface: 'IModel', // interface name
+                declareTo: 'Context.model', // declare to this interface
+                watch: true, // whether need to watch files
+                caseStyle: 'upper', // caseStyle for loader
+                interfaceHandle: Interface => `ReturnType<typeof ${Interface}>`, // interfaceHandle
+                trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
+            },
+            mongoose: {
+                directory: 'app/mongoose', // files directory.
+                pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
+                ignore: '', // ignore glob pattern, default to empty.
+                generator: 'function', // generator name, eg: class、auto、function、object
+                interface: 'IMongoose', // interface name
+                declareTo: 'Context.mongoose', // declare to this interface
+                watch: true, // whether need to watch files
+                caseStyle: 'upper', // caseStyle for loader
+                interfaceHandle: Interface => `ReturnType<typeof ${Interface}>`, // interfaceHandle
+                trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
+            },
+            service: {
+                directory: 'app/service', // files directory.
+                pattern: '**/*.(ts|js)', // glob pattern, default is **/*.(ts|js). it doesn't need to configure normally.
+                ignore: '', // ignore glob pattern, default to empty.
+                generator: 'class', // generator name, eg: class、auto、function、object
+                interface: 'IService', // interface name
+                declareTo: 'Context.service', // declare to this interface
+                watch: true, // whether need to watch files
+                caseStyle: 'lower', // caseStyle for loader
+                interfaceHandle: Interface => `${Interface}`, // Interface Handler
+                trigger: ['add', 'unlink'], // recreate d.ts when receive these events, all events: ['add', 'unlink', 'change']
+            }
+        },
+        autoRemoveJs: true,
+    });
 
-    return exports(ignores);
+    return exports(['generatorConfig.mongoose']);
 };
 
 /** @description - Immediately Run MicroTask. ~!*/
